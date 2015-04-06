559cd6a60a1689eab6ff9a578439d7a2ec0a24aa
otnorli <ole_n@hotmail.com>
2015-04-06 22:14:10 +0200

    Update Add_Person_Admin.php

--- Add_Person_Admin.php
+++ Add_Person_Admin.php
@@ -1,1 +1,21 @@
 
+<?php
+    
+require_once("add_ppl_class.php");
+
+// Get input
+if (isset($_GET['Navn'])){
+    $Input_Navn = $_GET['Navn'];
+    $Input_Alder = $_GET['Alder'];
+    $Input_Bil = $_GET['Bil'];
+
+    $class_object = new Database_Access();
+
+    // Pass input
+    $class_object->Set_Input($Input_Navn, $Input_Alder, $Input_Bil);
+
+    // Update database
+    $class_object->Update_Database();
+}
+
+?>
